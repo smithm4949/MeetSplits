86942c485cb51b85c1719db4e25261833bf49698
smithm4949 <smithm4949@gmail.com>
2023-04-19 12:59:08 -0400

    updating typeDefs to remove improper references

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,10 +5,9 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   type User {
     _id: ID
-    username: String
+    name: String
     email: String
     password: String
-    thoughts: [Thought]!
   }
 
   type Auth {
