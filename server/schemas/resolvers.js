289f13e19c6ee8549459e76dc0b51822c06666c6
smithm4949 <smithm4949@gmail.com>
2023-04-19 12:55:22 -0400

    adding auth to backend

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,45 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 // import models
-const { } = require('../models');
+const { User } = require('../models');
 
 // define our resolvers
 const resolvers = {
   // queries (read operations)
   Query: {
+    users: async () => {
+      return User.find();
+    },
+    user: async (parent, { email }) => {
+      return User.findOne({ email });
+    }
   },
+
   // mutations (write operations)
   Mutation: {
+    addUser: async (parent, { name, email, password }) => {
+      const user = await User.create({ name, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
   },
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,31 @@
 const { gql } = require('apollo-server-express');
 
-// definte our types here. Schema for objects that should match mongo schema
+// define our types here. Schema for objects that should match mongo schema
 // queries and mutations to match our resolvers
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    thoughts: [Thought]!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(email: String!): User
+  }
+
+  type Mutation {
+    addUser(name: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
+
 `;
 
 module.exports = typeDefs;
